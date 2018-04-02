349ea1f8eae7b3fcd320c51ac7f2eac48b83a99e
TheDayIsMyEnemy <sickskills91@gmail.com>
2018-04-02 17:32:24 +0300

    templating with Handlebars

---
+++ 03. JS Applications/06. Templating/app.js
@@ -0,0 +1,38 @@
+$(() => {
+
+    let details;
+    let data;
+
+    async function loadFiles() {
+        let contacts =  await $.get('templates/contacts.hbs');
+        let contactsTemplate = Handlebars.compile(contacts);
+
+        details = await $.get('templates/details.hbs');
+
+        data = await $.get('data.json');
+        let obj = {contacts: data};
+
+        let table = contactsTemplate(obj);
+
+        $('#list').append(table);
+        attachEvents();
+    }
+
+    loadFiles();
+
+    function attachEvents() {
+        $('.contact').on('click', function () {
+            loadDetails($(this).attr('data-id'));
+            $('.active').removeClass('active');
+            $(this).addClass('active');
+        })
+    }
+
+    function loadDetails(index) {
+        let detailsTemplate = Handlebars.compile(details);
+        let html = detailsTemplate(data[index]);
+        console.log(data[index]);
+        $('#details').empty();
+        $('#details').append(html);
+    }
+});
